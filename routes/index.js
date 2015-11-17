219fe2d02b25ce183477c62a95ac9773656b773c
Randall Degges <r@rdegges.com>
2015-11-17 13:47:24 -0800

    Adding an index route.

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+var router = express.Router();
+
+router.get('/', stormpath.loginRequired, function(req, res) {
+  res.send('Hello + ' + req.user.givenName + ', you are now logged in! Thanks for using your Yubikey!');
+});
+
+module.exports = router;
