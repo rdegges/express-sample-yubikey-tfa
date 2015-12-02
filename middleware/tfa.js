0d77ecc360231339f0ae5353e845fb32c8b59b48
Randall Degges <r@rdegges.com>
2015-12-02 10:51:37 -0800

    Fixing redirect issue by adding proper skipping logic.

--- middleware/tfa.js
+++ middleware/tfa.js
@@ -3,10 +3,21 @@
 /**
  * This middleware will redirect the user to the Yubikey registration page if
  * they haven't already registered a Yubikey device with their account.
+ *
+ * It will also redirect the user to the Yubikey login page if they do not have
+ * the appropriate field stored in customData.
  */
 module.exports = function(req, res, next) {
-  if (req.user && req.path !== '/connect' && !req.user.customData.yubikeyUserId) {
-    return res.redirect('/connect');
+  if (req.path === '/tfa/register' || req.path === '/tfa/login') {
+    return next();
+  }
+
+  if (req.user && req.path !== '/tfa/register' && !req.user.customData.yubikeyUserId) {
+    return res.redirect('/tfa/register');
+  }
+
+  if (req.user && req.path !== '/tfa/login' && !req.user.customData.yubikeySessionId) {
+    return res.redirect('/tfa/login');
   }
 
   next();
