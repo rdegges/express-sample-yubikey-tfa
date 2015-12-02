cce402bf0ca749c08e82d3f52594fba2217d4fba
Randall Degges <r@rdegges.com>
2015-12-01 17:21:21 -0800

    Updating text.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var stormpath = require('express-stormpath');
 var router = express.Router();
 
 router.get('/', stormpath.loginRequired, function(req, res) {
-  res.send('Hello + ' + req.user.givenName + ', you are now logged in! Thanks for using your Yubikey!');
+  res.send('Hello ' + req.user.givenName + ', you are now logged in! Thanks for using your Yubikey!');
 });
 
 module.exports = router;
