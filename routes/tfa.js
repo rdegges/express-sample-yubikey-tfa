cf2ac31f1c1134f01fe729c424b97e1f5cc90153
Randall Degges <r@rdegges.com>
2015-12-02 10:52:26 -0800

    Adding TFA routes.

--- routes/tfa.js
+++ routes/tfa.js
@@ -30,6 +30,7 @@ router.post('/register', stormpath.loginRequired, function(req, res) {
     var data = req.user.customData;
     var userId = otp.substring(0, 12);
 
+    data.yubikeySessionId = userId;
     data.yubikeyUserId = userId;
     data.save(function(err) {
       if (err) {
@@ -37,6 +38,41 @@ router.post('/register', stormpath.loginRequired, function(req, res) {
       }
 
       res.redirect(config.web.register.nextUri);
+    });
+  });
+});
+
+router.get('/login', stormpath.loginRequired, function(req, res) {
+  res.render('tfa-login');
+});
+
+router.post('/login', stormpath.loginRequired, function(req, res) {
+  var config = req.app.get('stormpathConfig');
+  var otp = req.body.otp;
+
+  if (!otp) {
+    return res.render('tfa-login');
+  }
+
+  yubikey.verify(otp, function(err) {
+    if (err) {
+      return res.render('tfa-login', { error: err });
+    }
+
+    var data = req.user.customData;
+    var userId = otp.substring(0, 12);
+
+    if (userId !== data.yubikeyUserId) {
+      return res.render('tfa-login', { error: new Error('Incorrect Yubikey device supplied.') });
+    }
+
+    data.yubikeySessionId = otp;
+    data.save(function(err) {
+      if (err) {
+        return res.render('tfa-login', { error: err });
+      }
+
+      res.redirect(config.web.login.nextUri);
     });
   });
 });
